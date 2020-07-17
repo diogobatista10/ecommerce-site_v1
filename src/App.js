3f981fce540179fb423a6de0dd06d0f40d27c434
Diogo Pinto Batista <diogo.pinto.batista@blip.pt>
2020-07-17 11:35:17 +0100

    Add sign in component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInOutPage from './pages/sign-in-out/sign-in-out.component';
 
 function App() {
   return (
-    <div >
+    <div className='app'>
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInOutPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-out/sign-in-out.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './sign-in-out.component.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignInOutPage = () => {
+    return <div className='sign-in-and-sign-out'>
+        <SignIn />
+    </div>
+}
+
+export default SignInOutPage;
